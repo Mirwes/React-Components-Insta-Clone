cf53c178ab02bd354552c733e0502fd2c84bcaa1
Mirwes <mirwes210@gmail.com>
2020-08-28 09:47:19 -0700

    Completed the Insta-Clone Project.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ Look at each file to see where you need to pass props or add code
 */
 import React from "react";
 import "./App.css";
+import SearchBar from "./components/SearchBar/SearchBarContainer";
+import LikeSection from "./components/PostsContainer/LikeSection";
+import Post from "./components/PostsContainer/PostsPage";
+import postHeader from "./components/PostsContainer/PostHeader";
+// import PostsPage from "./components/PostsContainer/PostsPage";
 // import the PostsPage and SearchBar and add them to the App
 
 
@@ -13,7 +18,13 @@ const App = () => {
 
   return (
     <div className="App">
+      <SearchBar />
+      <postHeader />
+      <Post />
+     
+      <LikeSection />
       {/* Add imported components here to render them */}
+
     </div>
   );
 };
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,17 +1,30 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
+import dummyData from "../../dummy-data";
 
-const PostsPage = () => {
+  const PostsPage = (props) => {
   // set up state for your data
+  const [posts, setPost] = useState(dummyData);
+   
   return (
+    
     <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
-    </div>
-  );
-};
+      {posts.map((post) => <Post post= {post} /> )}
+   
+         </div>
+      
+      )
+  }
+      
+      // /* map through data here to return a Post and pass data as props to Post *///
+     
+     
+     
+      
+
 
 export default PostsPage;
